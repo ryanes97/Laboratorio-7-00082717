7250a88d34091614ec9cd17ebdfc37d0922b87e2
ryanes97 <00082717@uca.edu.sv>
2018-10-12 09:11:15 -0600

    avance

--- asset/js/script.js
+++ asset/js/script.js
@@ -13,3 +13,29 @@ container.style.width = "200px";
 container.style.height = "200px";
 
 container.innerHTML = "<input type=”text” placeholder=’escriba su texto’>";
+
+/*Usando "document.getElementsByClassName"*/
+
+var containers = document.getElementsByClassName("c2");
+
+Array.prototype.forEach.call(containers, element => {
+    element.style.backgroundColor = "#009999";
+    element.innerHTML = "<input type='text' placeholder='escriba su texto'>";
+});
+
+/*Eventos*/
+
+var boton = document.getElementById("btn-click");
+
+boton.onclick = function(evt) 
+{ 
+alert("Hola mundo"); 
+}
+
+var btnCopy = document.getElementById("btnCopy");
+
+btnCopy.onclick = function(evt) 
+{ 
+let msj = document.getElementById('textMsj').value;
+document.getElementById('showMsj').innerText = msj;
+}
