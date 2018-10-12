22e65e2aeac3763a806a235d0d83ea77b8e76b28
ryanes97 <00082717@uca.edu.sv>
2018-10-12 08:32:08 -0600

    avance

---
+++ asset/js/script.js
@@ -0,0 +1,15 @@
+var container;
+
+container = document.getElementById("c1"); 
+
+container.innerText = "Hola Mundo!";
+
+container.style.backgroundColor = "#000000";
+
+container.style.color = "#1cb723";
+
+container.style.width = "200px";
+
+container.style.height = "200px";
+
+container.innerHTML = "<input type=”text” placeholder=’escriba su texto’>";
